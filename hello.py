8eb5e8b0a68801237fafaa75e453884da7417c5b
marinar27 <marinar7240@gmail.com>
2025-06-30 20:59:18 -0300

    update arquivo hello

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print("hello world");
+#print("hello world!")
+#nome = "leonardo"
+#print(nome)
+#outro_nome = "roblox"
+#print(outro_nome)
+
+nome = input("Informe o seu nome completo: ")
+print(nome)
